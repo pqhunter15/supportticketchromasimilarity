12437187063af2fcecf422ed69f3a4ec125678cd
pqhunter15 <pqhunter15@outlook.com>
2025-08-02 15:03:31 -0400

    Update supportticketssearch.py

--- supportticketssearch.py
+++ supportticketssearch.py
@@ -44,7 +44,11 @@ query = st.text_input("Enter your question:")
 
 def rewrite_query_hf(original_query, num_rewrites=2):
     url = "https://api-inference.huggingface.co/models/ramsrigouthamg/t5_paraphraser"
-    headers = {"Authorization": f"Bearer {st.secrets['HF_API_KEY']}"}
+     headers = {
+        "Authorization": f"Bearer {st.secrets['HF_API_KEY']}",
+        "Content-Type": "application/json"
+    }
+
 
     reworded = []
     for _ in range(num_rewrites):
